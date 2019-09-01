0afe28e0f72c30f8a04342f356d0b1e66d4f1fcd
Stefan Malcek <stefan.malcek1@gmail.com>
2019-09-01 18:06:28 +0200

    Add layout components.

---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,27 @@
+import React, { Fragment } from 'react';
+import Logo from '../../Logo/Logo';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import NavigationItems from '../NavigationItems/NavigationItems';
+import classes from './SideDrawer.module.css';
+
+const sideDrawer = props => {
+  const attachedClasses = [
+    classes.SideDrawer,
+    props.open ? classes.Open : classes.Close
+  ];
+  return (
+    <Fragment>
+      <Backdrop show={props.open} clicked={props.closed} />
+      <div className={attachedClasses.join(' ')}>
+        <div className={classes.Logo}>
+          <Logo />
+        </div>
+        <nav>
+          <NavigationItems />
+        </nav>
+      </div>
+    </Fragment>
+  );
+};
+
+export default sideDrawer;
---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+import DrawerToggler from '../SideDrawer/DrawerToggle/DrawerToggle';
+import classes from './Toolbar.module.css';
+
+const toolbar = props => (
+  <header className={classes.Toolbar}>
+    <DrawerToggler clicked={props.drawerToggleClicked} />
+    <div className={classes.Logo}>
+      <Logo />
+    </div>
+    <nav className={classes.DesktopOnly}>
+      <NavigationItems />
+    </nav>
+  </header>
+);
+
+export default toolbar;
