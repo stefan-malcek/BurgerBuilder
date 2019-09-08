8ac21c0db8a1938844ddb619d45e1442e21b2e09
Stefan Malcek <stefan.malcek1@gmail.com>
2019-09-08 23:42:56 +0200

    Implement custom hook.

--- src/hoc/WithErrorHandler/WithErrorHandler.js
+++ src/hoc/WithErrorHandler/WithErrorHandler.js
@@ -1,36 +1,14 @@
-import React, { Fragment, useEffect, useState } from 'react';
+import React, { Fragment } from 'react';
 import Modal from '../../components/UI/Modal/Modal';
+import useHttpErrorHandler from '../../hooks/http-error-handler';
 
 const withErrorHandler = (WrappedComponent, axios) => {
   return props => {
-    const [error, setError] = useState(null);
-
-    const reqIntercepter = axios.interceptors.request.use(req => {
-      setError(null);
-      return req;
-    });
-
-    const resIntercepter = axios.interceptors.response.use(
-      res => res,
-      error => {
-        setError(error);
-      }
-    );
-
-    useEffect(() => {
-      return () => {
-        axios.interceptors.request.eject(reqIntercepter);
-        axios.interceptors.response.eject(resIntercepter);
-      };
-    }, [reqIntercepter, resIntercepter]);
-
-    const errorConfirmedHandler = () => {
-      setError(null);
-    };
+    const [error, clearError] = useHttpErrorHandler(axios);
 
     return (
       <Fragment>
-        <Modal show={error} modalClosed={errorConfirmedHandler}>
+        <Modal show={error} modalClosed={clearError}>
           {error ? error.message : null}
         </Modal>
         <WrappedComponent {...props} />
---
+++ src/hooks/http-error-handler.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react';
+
+export default httpClient => {
+  const [error, setError] = useState(null);
+
+  const reqIntercepter = httpClient.interceptors.request.use(req => {
+    setError(null);
+    return req;
+  });
+
+  const resIntercepter = httpClient.interceptors.response.use(
+    res => res,
+    error => {
+      setError(error);
+    }
+  );
+
+  useEffect(() => {
+    return () => {
+      httpClient.interceptors.request.eject(reqIntercepter);
+      httpClient.interceptors.response.eject(resIntercepter);
+    };
+  }, [reqIntercepter, resIntercepter]);
+
+  const errorConfirmedHandler = () => {
+    setError(null);
+  };
+
+  return [error, errorConfirmedHandler];
+};
