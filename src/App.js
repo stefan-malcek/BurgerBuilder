122581a1eb616d3023234735b73826f8f3ccd55e
Stefan Malcek <stefan.malcek1@gmail.com>
2019-09-09 11:40:40 +0200

    Fix lazy routing.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React, { Component, Suspense } from 'react';
 import { connect } from 'react-redux';
 import { Redirect, Route, Switch } from 'react-router-dom';
-import Auth from './containers/Auth/Auth';
 import Logout from './containers/Auth/Logout/Logout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Layout from './hoc/Layout/Layout';
 import * as actions from './store/actions/index';
 
-const AsyncOrders = React.lazy(() => import('./containers/Orders/Orders'));
-const AsyncCheckout = React.lazy(() =>
-  import('./containers/Checkout/Checkout')
-);
+const Auth = React.lazy(() => import('./containers/Auth/Auth'));
+const Checkout = React.lazy(() => import('./containers/Checkout/Checkout'));
+const Orders = React.lazy(() => import('./containers/Orders/Orders'));
 
 class App extends Component {
   componentDidMount() {
@@ -20,7 +18,7 @@ class App extends Component {
   render() {
     let routes = (
       <Switch>
-        <Route path="/auth" component={Auth} />
+        <Route path="/auth" render={props => <Auth {...props} />} />
         <Route path="/" exact component={BurgerBuilder} />
         <Redirect to="/" />
       </Switch>
@@ -29,23 +27,9 @@ class App extends Component {
     if (this.props.isAuthenticated) {
       routes = (
         <Switch>
-          <Route
-            path="/checkout"
-            render={() => (
-              <Suspense fallback={<div>Loading...</div>}>
-                <AsyncCheckout />
-              </Suspense>
-            )}
-          />
-          <Route
-            path="/orders"
-            render={() => (
-              <Suspense fallback={<div>Loading...</div>}>
-                <AsyncOrders />
-              </Suspense>
-            )}
-          />
-          <Route path="/auth" component={Auth} />
+          <Route path="/checkout" render={props => <Checkout {...props} />} />
+          <Route path="/orders" render={props => <Orders {...props} />} />
+          <Route path="/auth" render={props => <Auth {...props} />} />
           <Route path="/logout" component={Logout} />
           <Route path="/" exact component={BurgerBuilder} />
           <Redirect to="/" />
@@ -55,7 +39,9 @@ class App extends Component {
 
     return (
       <div>
-        <Layout>{routes}</Layout>
+        <Layout>
+          <Suspense fallback={<div>Loading...</div>}>{routes}</Suspense>
+        </Layout>
       </div>
     );
   }
